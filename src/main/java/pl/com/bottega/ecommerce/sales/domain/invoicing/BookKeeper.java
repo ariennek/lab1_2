0010556e323c7c04969f1a32bbd0d22c11c18c27
Dudek.Piotr <194936@edu.p.lodz.pl>
2017-03-12 15:52:20 +0100

    Poprawienie metody issuance

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -29,7 +29,7 @@ public class BookKeeper {
 	public Invoice issuance(InvoiceRequest invoiceRequest, List<RequestItem> items) {
 		Invoice invoice = invoiceCreator.create(invoiceRequest.getClientData());
 
-		for (RequestItem item : items) {
+		for (RequestItem item : invoiceRequest.getItems()) {
 			Money net = item.getTotalCost();
 			BigDecimal ratio = null;
 			String desc = null;
