f57311ea6331e8016d7eaed0bf7cec2ec834334e
turkowska.katarzyna <195079@edu.p.lodz.pl>
2017-03-13 21:59:19 +0100

    add new package tax and move there Tax class

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -16,10 +16,9 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import java.math.BigDecimal;
-import java.util.List;
 
-import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.tax.Tax;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
