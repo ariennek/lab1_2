a9e6d5191c583da5aaec93e1404818f9c07bb249
zimny.krzysztof <180318@edu.p.lodz.pl>
2017-03-19 05:20:16 +0100

    Refaktoryzacja sposobu tworzenia klasy Invoice, utworzenie fabryki InvoiceFactory, poprawa formatowania oraz usuniecie nieuzywanych importow
    Signed-off-by: zimny.krzysztof <180318@edu.p.lodz.pl>

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactory.java
@@ -0,0 +1,15 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
+/**
+ * Created by Krzysztof Zimny on 3/19/2017.
+ */
+public class InvoiceFactory {
+
+    public static Invoice createInvoice(ClientData clientData) {
+        Id id = Id.generate();
+        return new Invoice(id, clientData);
+    }
+}
